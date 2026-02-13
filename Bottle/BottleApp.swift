13c3c067d0e66b238932cbed77ddb18522c5e19f
RZ <rzabasajja@gmail.com>
2026-02-13 15:01:07 -0800

    Initial Commit

---
+++ Bottle/BottleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleApp.swift
+//  Bottle
+//
+//  Created by Raina Zab on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BottleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
